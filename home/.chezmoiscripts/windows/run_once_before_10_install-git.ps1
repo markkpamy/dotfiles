77822bffe68d53d8783b00665cad5558bfc9da24
Mark Kpamy <markkpamy@gmail.com>
2025-09-25 10:42:14 +0200

    feat(scripts): reorder and elevate install checks for Scoop and Git
    - Moved Scoop installation check and path initialization to occur after configuration validation.
    - Added administrator self-elevation logic for Git installation.

--- home/.chezmoiscripts/windows/run_once_before_10_install-git.ps1
+++ home/.chezmoiscripts/windows/run_once_before_10_install-git.ps1
@@ -1,1 +1,14 @@
-winget install --id Git.Git
+if (-not (Get-Command "git" -ErrorAction SilentlyContinue))
+{
+    # Self-elevate the script if required
+    if (-Not ([Security.Principal.WindowsPrincipal] [Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole] 'Administrator'))
+    {
+        if ([int](Get-CimInstance -Class Win32_OperatingSystem | Select-Object -ExpandProperty BuildNumber) -ge 6000)
+        {
+            $CommandLine = "-File `"" + $MyInvocation.MyCommand.Path + "`" " + $MyInvocation.UnboundArguments
+            Start-Process -Wait -FilePath pwsh.exe -Verb Runas -ArgumentList $CommandLine
+            Exit
+        }
+    }
+    winget install --id Git.Git
+}
